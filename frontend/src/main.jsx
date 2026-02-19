a810269e0746dbacbebcc1230d595ee84d1addcb
Amit Rajput <amitrajput98267313@gmail.com>
2026-02-19 12:53:45 +0530

    style: update global styles and main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { FiltersProvider } from "./context/FiltersContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <FiltersProvider>
+  <React.StrictMode>
     <App />
-  </FiltersProvider>
+  </React.StrictMode>
 );
